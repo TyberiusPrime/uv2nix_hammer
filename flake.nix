d9e6b4d7928ca7b14de6eb0ae3068da2520a4ee9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-04 08:27:07 +0200

    added a flake for dev

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    nixpkgs.url = "github:/nixos/nixpkgs/master";
+  };
+  outputs = {
+    systems,
+    treefmt-nix,
+    nixpkgs,
+    ...
+  }: let
+    eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+    treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./dev/treefmt.nix);
+    inherit (nixpkgs) lib;
+  in {
+    formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+    devShell = eachSystem (pkgs:
+      pkgs.mkShell {
+        buildInputs = [pkgs.uv (pkgs.python312.withPackages (p: [p.rich p.packaging p.toml p.urllib3]))];
+      });
+  };
+}
