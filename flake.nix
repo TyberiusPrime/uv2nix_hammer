545ea9b568c54336a7af4f771f99116b91b0c401
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2025-05-08 07:59:09 +0200

    provide an app for nix run

--- flake.nix
+++ flake.nix
@@ -4,16 +4,17 @@
     nixpkgs.url = "github:/nixos/nixpkgs/master";
   };
   outputs = {
+    self,
     systems,
     treefmt-nix,
     nixpkgs,
     ...
   }: let
     eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
-    treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./dev/treefmt.nix);
-    inherit (nixpkgs) lib;
+    # treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./dev/treefmt.nix);
+    # inherit (nixpkgs) lib;
   in {
-      # formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+    # formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
     devShell = eachSystem (pkgs:
       pkgs.mkShell {
         buildInputs = [pkgs.uv pkgs.rsync (pkgs.python312.withPackages (p: [p.rich p.packaging p.toml p.urllib3]))];
@@ -24,8 +25,16 @@
           name = "uv2nix_hammer";
           format = "pyproject";
           src = ./.;
-            nativeBuildInputs = [pkgs.python312Packages.hatchling];
+          nativeBuildInputs = [pkgs.python312Packages.hatchling];
           propagatedBuildInputs = with pkgs.python312Packages; [rich packaging toml urllib3 networkx];
+        };
+      }
+    );
+    apps = eachSystem (
+      pkgs: {
+        default = {
+          type = "app";
+          program = "${self.packages.${pkgs.system}.default}/bin/uv2nix-hammer";
         };
       }
     );
